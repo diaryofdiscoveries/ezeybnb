35c0dd97d82569e1225a44b4df08340c7f1b03e8
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-10-04 12:07:50 +0100

    Feature tested homepage with zombie and mocha

--- app.js
+++ app.js
@@ -46,3 +46,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+if (!module.parent) {
+  http.createServer(app).listen(process.env.PORT, function(){
+    console.log("Server listening on port " + app.get('port'));
+  });
+}
---
+++ test/functional/homepageSpec.js
@@ -0,0 +1,32 @@
+// force the test environment to 'test'
+process.env.NODE_ENV = 'test';
+var http = require('http');
+// get the application server module
+var app = require('../../app');
+// use zombie.js as headless browser
+var Browser = require('zombie');
+var assert = require('assert');
+
+describe('homepage', function() {
+
+  before(function() {
+    this.server = http.createServer(app).listen(3000);
+    // initialize the browser using the same port as the test application
+    this.browser = new Browser({ site: 'http://localhost:3000' });
+  });
+
+  // load the contact page
+  before(function(done) {
+    this.browser.visit('/', done);
+  });
+
+  it('should welcome to Ezey BnB', function(){
+    assert.ok(this.browser.success);
+    assert.equal(this.browser.text('h1'), 'Ezey BnB');
+    assert.equal(this.browser.text('p'), 'Welcome to Ezey BnB');
+  });
+
+  after(function(done) {
+    this.server.close(done);
+  });
+});
